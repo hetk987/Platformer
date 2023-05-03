81d4970eaf8293ce1cbcd0c5691464bf0553be97
hetk987 <66132321+hetk987@users.noreply.github.com>
2023-05-03 17:54:08 -0500

    Finished the initial Threading of the gameloop

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,15 +1,50 @@
 package main;
 
-public class Game {
+public class Game implements Runnable{
     private GameWindow gameWindow;
     private GamePanel gamePanel;
-
+    private Thread gameThread;
+    private final int FPS_SET = 120;
     public Game(){
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.setFocusable(true);
         gamePanel.requestFocus(true);
+        StartGameLoop();
         
+    }
+
+    @Override
+    public void run() {
+        //time per frame in nanoseconds
+        double timePerFrame = 1000000000.0 / FPS_SET;
+		long lastFrame = System.nanoTime();
+		long now = System.nanoTime();
+
+		int frames = 0;
+		long lastCheck = System.currentTimeMillis();
+
+		while (true) {
+
+			now = System.nanoTime();
+			if (now - lastFrame >= timePerFrame) {
+				gamePanel.repaint();
+				lastFrame = now;
+				frames++;
+			}
+
+			if (System.currentTimeMillis() - lastCheck >= 1000) {
+				lastCheck = System.currentTimeMillis();
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+        }
+    }
+
+
+    private void StartGameLoop(){
+        gameThread = new Thread(this);
+        gameThread.start();
     }
     
 }
