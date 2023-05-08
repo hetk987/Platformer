2d4632a373a7f0c46d72cde2562528ea10835ea9
hetk987 <66132321+hetk987@users.noreply.github.com>
2023-05-08 14:46:53 -0500

    basic Animation of the man running

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,27 +1,67 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import inputs.KeyBoardInputs;
 import inputs.MouseInputs;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.Random;
-
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class GamePanel extends JPanel{
 
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
+    private BufferedImage img;
+    private BufferedImage[] animation;
+    private int animationIndex = 0;
+    private int animationTick=0;
+    private int animationSpeed = 20;
 
     public GamePanel(){
+
+        importImage();
+        loadAnimation();
+
 
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyBoardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+        setPanelSize();
     }
+    
+
+    private void loadAnimation() {
+        animation = new BufferedImage[4];
+        for(int i = 0; i<animation.length; i++){
+            animation[i] = img.getSubimage(i*100, 0, 100, 100);
+        }
+
+    }
+
+
+    private void importImage() {
+        
+        
+        try {
+            img = ImageIO.read(new FileInputStream("res/diverSpriteAtlas.png"));
+        } catch (IOException e) {
+            System.out.println("Reading Image Error");
+            e.printStackTrace();
+        }
+        
+    }
+
+
+    private void setPanelSize() {
+        setPreferredSize(new Dimension(1500, 900));
+    }
+
 
     //Changes the position when WASD or Arrows are used
     public void changeXDelta(int num){
@@ -38,7 +78,23 @@ public class GamePanel extends JPanel{
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        repaint();
+        updateAnimationTick();
+        g.drawImage(animation[animationIndex], (int) xDelta, (int) yDelta, 200, 200, getFocusCycleRootAncestor());
+    }
+
+
+    private void updateAnimationTick() {
+        animationTick++;
+        if(animationTick >= animationSpeed){
+            animationIndex++;
+            animationTick = 0;
+            if(animationIndex>=animation.length)
+            {
+                animationIndex = 0;
+            }
+
+        }
+           
     }
 
 
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -8,9 +8,11 @@ public class GameWindow {
     public GameWindow(GamePanel gamePanel){
         jframe = new JFrame();
 
-        jframe.setSize(400, 400);   
+         
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.add(gamePanel);
+        jframe.pack();
+        jframe.setResizable(false);
         jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
     }
